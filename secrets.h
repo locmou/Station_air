49f61c5a2a438438bccd4fbb5d07f6abf995a594
locmou <124572531+locmou@users.noreply.github.com>
2026-03-27 16:33:49 +0100

    Create secrets.h
    Ajout file secrets

---
+++ secrets.h
@@ -0,0 +1,18 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// ===== WIFI =====
+#define WIFI_SSID_1 "Mounwiff"
+#define WIFI_PASS_1 "rue_de_la_Grande680Plage_10!"
+
+// Tu peux en ajouter d'autres si besoin
+//#define WIFI_SSID_2 "AutreWifi"
+//#define WIFI_PASS_2 "motdepasse"
+
+// ===== MQTT =====
+#define MQTT_SERVER "192.168.1.11"
+#define MQTT_PORT 1883
+#define MQTT_USER "loic.mounier@laposte.net"
+#define MQTT_PASS "vgo:?2258H"
+
+#endif
